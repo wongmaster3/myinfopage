2a49af935bdc3bc0d6aef3b4b6f89811e099a993
Matt Wong <MattWong@Matts-MacBook-Pro-2.local>
2020-08-20 10:10:02 -0400

    Created deployment folder

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('../docs'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: '../docs'}
+  );
+  });
+app.listen(process.env.PORT || 8080);
